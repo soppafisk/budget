95eff9799d4eca6438e53b5a8a16001585646f7b
Jens <soppafisk@gmail.com>
2016-07-07 18:30:12 +0000

    added validation to add receipts

--- resources/assets/js/components/plan/AddForm.js
+++ resources/assets/js/components/plan/AddForm.js
@@ -3,6 +3,29 @@ import { connect } from 'react-redux'
 import { reduxForm } from 'redux-form'
 import { addReceipt } from '../../actions'
 
+const validate = values => {
+    const errors = {}
+    console.log(values)
+
+    if (!values.user_id) {
+        errors.user_id = "Välj användare"
+    }
+
+    if (!values.amount) {
+        errors.amount = "Ange summa"
+    }
+
+    if (!values.buy_date) {
+        errors.buy_date = "Ange datum"
+    }
+
+    if (!values.store) {
+        errors.store = "Ange butik"
+    }
+
+    return errors
+}
+
 var AddForm = React.createClass({
     handleSubmit: function(newReceipt) {
         let {
@@ -36,17 +59,21 @@ var AddForm = React.createClass({
                     <div className="form-group">
                         <label htmlFor="amount">Summa:</label>
                         <input type="text" name="amount" className="form-control" {...amount} />
+                        { amount.touched && amount.error }
                     </div>
                     <div className="form-group">
                         <label htmlFor="store">Butik:</label>
                         <input type="text" name="store" className="form-control autocomplete" {...store}/>
+                        { store.touched && store.error }
                     </div>
                     <div className="form-group">
                         <label htmlFor="buy_date">Köpdatum:</label>
                         <input type="date" name="buy_date" className="form-control" {...buy_date} />
+                        { buy_date.touched && buy_date.error }
                     </div>
                     <div className="form-group">
                         { userRadioButtons }
+                        { user_id.touched && user_id.error }
                     </div>
                     <div className="form-group">
                         <label htmlFor="comment">Kommentar:</label>
@@ -67,6 +94,7 @@ var AddForm = React.createClass({
 AddForm = reduxForm({
     form: 'addReceiptForm',
     fields: ['amount', 'store', 'buy_date', 'user_id', 'comment'],
+    validate,
 },
 state => ({
     initialValues: state.newReceipt,
