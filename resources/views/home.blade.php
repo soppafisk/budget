6d6ddd956d31cd4d0559170103a67b7cb3e08a02
Jens Pettersson <soppafisk@gmail.com>
2016-07-18 22:11:23 +0200

    adjusted views

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,17 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-                {!! Form::open(array('action' => ['UserController@setColor', $user->id])) !!}
-                    {{ Form::input('color', 'user_color', $user->color, ['class' => 'form-control']) }}
-                    {{ Form::submit('Ändra färg', ['class' => 'form-control']) }}
-                {!! Form::close() !!}
-                <div class="panel-body">
-                    You are logged in!
+            <div class="row">
+                <div class="col-xs-12 col-md-6">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Din färg</div>
+                        <div class="panel-body">
+                            {!! Form::open(array('action' => ['UserController@setColor', $user->id])) !!}
+                                {{ Form::input('color', 'user_color', $user->color, ['class' => 'form-control']) }}
+                                {{ Form::submit('Ändra färg', ['class' => 'form-control']) }}
+                            {!! Form::close() !!}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
